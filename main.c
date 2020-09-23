c3678e5c75e7c8db4a3d0bd537884b4b9a59a8dd
Akbar Umar <akbarumar88@gmail.com>
2020-09-24 05:42:07 +0700

    First Commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Nama    : Akbar\n");
+    printf("NPM     : 19081010043\n");
+    printf("Fakultas: Ilmu Komputer\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Gender  : Laki-laki\n");
+    return 0;
+}
